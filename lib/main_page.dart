20dbd2215e7f35f969404d2ecb8eb20d868e9dc8
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-03-12 22:12:12 +0900

    main) kakaomap

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_kakao_map/flutter_kakao_map.dart';
+import 'package:flutter_kakao_map/kakao_maps_flutter_platform_interface.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -10,11 +12,30 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  late KakaoMapController kakaoMapController;
+  MapPoint _mapPoint = const MapPoint(37, 127);
+  CameraPosition _cameraPosition =
+      const CameraPosition(target: MapPoint(37, 127), zoom: 5);
+
+  void onMapCreated(KakaoMapController controller) async {
+    final MapPoint mapPoint = await controller.getMapCenterPoint();
+    setState(() {
+      kakaoMapController = controller;
+      _mapPoint = mapPoint;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Main')),
-      body: Center(),
+      body: Center(
+        child: SizedBox(
+          width: 300,
+          height: 200,
+          child: KakaoMap(initialCameraPosition: _cameraPosition),
+        ),
+      ),
     );
   }
 }
