b267fca080d1d2c0934ebb39c8fdcd7a3934f640
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-03-13 21:49:30 +0900

    null safety

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:flutter_kakao_map/flutter_kakao_map.dart';
-import 'package:flutter_kakao_map/kakao_maps_flutter_platform_interface.dart';
+import 'package:get/get.dart';
+import 'package:kakao_map_flutter/kakao_map_flutter.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -12,18 +12,18 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  late KakaoMapController kakaoMapController;
-  MapPoint _mapPoint = const MapPoint(37, 127);
-  CameraPosition _cameraPosition =
-      const CameraPosition(target: MapPoint(37, 127), zoom: 5);
+  late KakaoMapController _kakaoMapController;
+  // MapPoint _mapPoint = const MapPoint(37, 127);
+  // CameraPosition _cameraPosition =
+  //     const CameraPosition(target: MapPoint(37, 127), zoom: 5);
 
-  void onMapCreated(KakaoMapController controller) async {
-    final MapPoint mapPoint = await controller.getMapCenterPoint();
-    setState(() {
-      kakaoMapController = controller;
-      _mapPoint = mapPoint;
-    });
-  }
+  // void onMapCreated(KakaoMapController controller) async {
+  //   final MapPoint mapPoint = await controller.getMapCenterPoint();
+  //   setState(() {
+  //     kakaoMapController = controller;
+  //     _mapPoint = mapPoint;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -35,9 +35,33 @@ class _MainPageState extends State<MainPage> {
         ),
         backgroundColor: Colors.black,
       ),
-      body: Center(
-        child: KakaoMap(initialCameraPosition: _cameraPosition),
-      ),
+      body: SafeArea(
+          child: Column(children: [
+        Expanded(
+            child: KakaoMap(
+          initLocation: KakaoLatLng(33.450701, 126.570667),
+          kakaoApiKey: "0bbb8627599c1198a2cd41317f240032",
+          clustererServiceEnable: true,
+          onMapCreated: (controller) {
+            _kakaoMapController = controller;
+          },
+          onMapLoaded: () {
+            Get.rawSnackbar(
+                message: "맵 로드 완료",
+                margin: const EdgeInsets.all(8),
+                borderRadius: 12.0,
+                snackPosition: SnackPosition.TOP);
+            _kakaoMapController.setNowLocation();
+          },
+          onMarkerTouched: (markerLocation, markerIndex) {
+            Get.rawSnackbar(
+                message: "$markerLocation, $markerIndex",
+                margin: const EdgeInsets.all(8),
+                borderRadius: 12.0,
+                snackPosition: SnackPosition.TOP);
+          },
+        )),
+      ])),
     );
   }
 }
